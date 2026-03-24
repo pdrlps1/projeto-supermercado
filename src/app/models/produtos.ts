992b5bbe3f2816ac646bb9f495bd093098f13af1
pdrlps1 <pedro.otavio-12@outlook.com>
2026-03-24 19:26:49 -0300

    first commit

---
+++ src/app/models/produtos.ts
@@ -0,0 +1,9 @@
+export class Produtos {
+    id?: number;
+    marca?: string;
+    nome?: string;
+    precoCusto?: number;
+    precoVenda?: number;
+    categoria?: string;
+    estoque?: number = 0;
+}
